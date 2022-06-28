91cad3d5c45d7660b1e1b56c49085cb1a2f11ec2
Waswas94 <wassim.mahroug@gmail.com>
2022-06-28 14:46:00 +0200

    test

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -1,11 +1,11 @@
 
-const index = require("../app");
-const request = require("supertest");
-const express = require("express");
-const app = express();
+// const index = require("../app");
+// const request = require("supertest");
+// const express = require("express");
+// const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use("/", index);
+// app.use(express.urlencoded({ extended: false }));
+// app.use("/", index);
 
 // test("index route works", done => {
 //   request(app)
@@ -15,4 +15,26 @@ app.use("/", index);
 //     .expect(200, done);
 // });
 
-test('should first', () => { console.log('test')})
+// test('should first', () => { console.log('test')})
+
+const request = require("supertest");
+const app = require("../routes/api");
+
+describe("Test example", () => {
+    test('GET /', (done) => { 
+        request(app)
+        .get("/me")
+        .expect("Content-Type", /json/)
+        .expect({ message: "The api is working !" })
+        .expect(200, done)
+        .end((err, res) => {
+            if (err) return done(err);
+            return done();
+          });
+//     .expect({ message: "The api is working !" })
+//     .expect(200, done);
+
+     })
+  });
+
+// More things come after this
